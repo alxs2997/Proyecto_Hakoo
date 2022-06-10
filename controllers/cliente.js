3bc3bc736507dce1922e7c704c81696556828227
galle997 <98421465+galle997@users.noreply.github.com>
2022-06-09 22:46:46 -0600

    Arreglando modelo cliente y copiando codigo a controlador

--- controllers/cliente.js
+++ controllers/cliente.js
@@ -1,1 +1,8 @@
 //controlador de clientes
+const registro = Cliente.build({
+    Nombres: "María Magnolia", Apellidos: "Santos Benitez",
+    Nombre_Usuario: "JBP7", Correo: "correo@mail.com", Telefono: "23456789",
+    Password: "1234567"
+});
+
+await registro.save()
--- models/cliente.js
+++ models/cliente.js
@@ -36,10 +36,6 @@ const Cliente = sequelize.define('Cliente', {
 await Cliente.sync();
 
 
-const registro = Cliente.build({
-    Nombres: "María Magnolia", Apellidos: "Santos Benitez",
-    Nombre_Usuario: "JBP7", Correo: "correo@mail.com", Telefono: "23456789",
-    Password: "1234567"
-});
 
-await registro.save()
+
+export default Cliente
