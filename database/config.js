3efd211951e9fd7a69743dba5cb287d2e8c2f25f
galle997 <98421465+galle997@users.noreply.github.com>
2022-06-09 22:41:45 -0600

    Creando conexión a base de datos, utilizando archivos env, utilizando modelo de cliente e ingresando datos a la base de datos

--- database/config.js
+++ database/config.js
@@ -1,5 +1,26 @@
-import 'dotenv/config' 
+import 'dotenv/config'
+import { Sequelize } from 'sequelize';
+
+const user = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+const port = process.env.DB_PORT
+const url = process.env.DB_URL
+const database = process.env.DB_NAME
+
 
 //Aquí se hará la conexión en sequelize
 
-const user = process.env.DB_USERNAME 
+const sequelize = new Sequelize(database, user, password,{
+    host: url,
+    port: port,
+    dialect: 'mariadb'
+});
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+
+export default sequelize
--- models/cliente.js
+++ models/cliente.js
@@ -1,2 +1,45 @@
 //modelo de tabla de bd hakoo tabla cliente
+import sequelize from '../database/config.js'
 
+//Esquema de la tabla
+import { Sequelize, DataTypes } from 'sequelize';
+//const sequelize = new Sequelize('sqlite::memory:');
+const Cliente = sequelize.define('Cliente', {
+    // Model attributes are defined here
+    Nombres: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Apellidos: {
+        type: DataTypes.STRING
+        // allowNull defaults to true
+    },
+    Nombre_Usuario: {
+        type: DataTypes.STRING
+    },
+    Correo: {
+        type: DataTypes.STRING
+    },
+    Telefono: {
+        type: DataTypes.INTEGER
+    },
+    Password: {
+        type: DataTypes.STRING
+    },
+
+}, {
+    // Other model options go here
+});
+
+
+//Lo que hace sync (sincroniza los campos creados aca con la tabla de la BD)
+await Cliente.sync();
+
+
+const registro = Cliente.build({
+    Nombres: "María Magnolia", Apellidos: "Santos Benitez",
+    Nombre_Usuario: "JBP7", Correo: "correo@mail.com", Telefono: "23456789",
+    Password: "1234567"
+});
+
+await registro.save()
