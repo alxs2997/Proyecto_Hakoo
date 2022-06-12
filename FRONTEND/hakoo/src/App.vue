40a32752d689bc6f413a07d282a792dfdb9431ed
galle997 <98421465+galle997@users.noreply.github.com>
2022-06-12 16:16:22 -0600

    cambiando la versión de Vue.js de 3 a 2

---
+++ FRONTEND/hakoo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+        <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-collapse id="nav-collapse" is-nav>
+           <b-navbar-nav>
+             <b-nav-item href="#">
+                <router-link to="/">
+                </router-link> 
+              </b-nav-item>
+              <b-nav-item href="#" class="mr-sm-6">
+              </b-nav-item>
+           </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav>
+        <b-nav-item-dropdown text="Integrantes:" right>
+          <b-dropdown-item href="#">David Alexis Gallegos González</b-dropdown-item>
+          <b-dropdown-item href="#">Nestor Wilfredo Chavez Ramirez</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
